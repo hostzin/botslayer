ce7c0209533f7b6e0c4f4c64ee53ced86beb18dc
hostzin <79783671+hostzin@users.noreply.github.com>
2021-04-27 15:11:23 -0300

    Update numowner.js

--- numowner.js
+++ numowner.js
@@ -4,7 +4,7 @@ const vcard = 'BEGIN:VCARD\n'
             + 'VERSION:3.0\n' 
             + 'FN:Meu criador^~^\n' 
             + 'ORG:Ian;\n' 
-            + 'TEL;type=CELL;type=VOICE;waid=557187645787:+55 71 8764-5787\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=558681467600: +55 86 8146-7600 \ n' 
             + 'END:VCARD'
 
 exports.vcard = vcard
